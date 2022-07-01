51d1a232c5e67d4e01b1a30dd88d77bed5d40946
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 18:03:56 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -5,10 +5,17 @@ const app = express();
 app.use('/public', express.static(__dirname +'/public'));
 
 //notice can't do __dirname + '/views/index.html' must do '/views/' + 'index.html'
+// also notice ; needed insinde the routeHandler since it is a function(return)
 app.get("/", function routeHandler(req,res) { res.sendFile(__dirname + '/views/' + 'index.html');});
 
 
-app.get("/json", function routeHandler(req, res) { res.json( { message: "Hello json"});});
+//NOTICE NEED ; AFTER res.json CALL SINCE IT IS CODE IN A CALLBACK FUNCTION
+app.get("/json", function routeHandler(req, res) {
+    if (process.env.MESSAGE_STYLE == uppercase) {
+        res.json( { message: "HELLO JSON"});
+    else {
+        res.json({message: "Hello json"});
+    }});
 
 
 
