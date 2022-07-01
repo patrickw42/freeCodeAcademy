2247a8b2b90c91fa78135be95231e03eae51c787
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 16:24:37 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ console.log("Hello World");
 let express = require('express');
 let app = express();
 let path = require('path');
-app.get("/", function routeHandler(req,res) { res.sendFile(__dirname + '/views/" + index.html'});
+//notice can't do
+app.get("/", function routeHandler(req,res) { res.sendFile(__dirname + '/views/index.html'});
 
 
 
