2739602a53688276ade1497d3f7e2f43be611658
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 18:44:30 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 //must be single quotes which is odd
-require('dotenv').config();
+require("dotenv").config();
 
 console.log(process.env);
 const express = require("express");
@@ -14,11 +14,10 @@ app.get("/", function routeHandler(req, res) {
 });
 
 //NOTICE NEED ; AFTER res.json CALL SINCE IT IS CODE IN A CALLBACK FUNCTION
-//app.get("/json", function routeHandler(req, res) {
- if (process.env.MESSAGE_STYLE === "uppercase")
-   res.json({ message: "HELLO JSON" });
- else
-    res.json({ message: "Hello json" });
+app.get("/json", function routeHandler(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase")
+    res.json({ message: "HELLO JSON" });
+  else res.json({ message: "Hello json" });
 });
 
 module.exports = app;
