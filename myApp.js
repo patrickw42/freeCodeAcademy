db7cafafa31b7fff5086b47de6ef29a0bc2ad9d0
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 16:23:51 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -2,9 +2,7 @@ console.log("Hello World");
 let express = require('express');
 let app = express();
 let path = require('path');
-//notice must join each section of path. can join(__dirname,  /view/index.html)
-//let abpath = path.join(__dirname, 'views', 'index.html');
-app.get("/", function routeHandler(req,res) { res.sendFile('views/index.html', {root:__dirname})});
+app.get("/", function routeHandler(req,res) { res.sendFile(__dirname + '/views/" + index.html'});
 
 
 
