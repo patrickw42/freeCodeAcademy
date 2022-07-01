3aca1800fb834aeae482e13eefeb2e5c091c44e9
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 16:17:03 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ let app = express();
 let path = require('path');
 //notice must join each section of path. can join(__dirname,  /view/index.html)
 let abpath = path.join(__dirname, '/views', 'index.html');
-console.log(path);
+console.log(abpath);
 app.get("/", function routeHandler(req,res) { res.sendFile(abpath)});
-return path;
+return abpath;
 
 
 
