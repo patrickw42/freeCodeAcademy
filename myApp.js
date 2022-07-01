51d4c0479e57196f5321c504acdd591900ef0599
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 16:53:29 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 let path = require('path');
 //notice can't do __dirname + '/views/index.html' must do '/views/' + 'index.html'
 app.get("/", function routeHandler(req,res) { res.sendFile(__dirname + '/views/' + 'index.html')});
-app.use(app.static__dirname + "/public"));
+app.use(app.static(__dirname + "/public"));
 
 
 
