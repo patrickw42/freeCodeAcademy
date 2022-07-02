dbd4f1cf86d1ce29e722677e75a092af6e4c06fc
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-01 20:10:58 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ const app = express();
 
 //middleware function takes 3 args request result and next function to call. if no next() would infitloop
 app.use("/", (req, res, next) => {
-  console.log(req.method, " ", req.path, " - ", req.ip);
+  console.log("req.method + ' ' + req.path + ' - ' + req.ip");
   next();
 });
 
