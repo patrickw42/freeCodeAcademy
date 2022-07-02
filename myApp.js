00a0fe85fce3f4aaddc567637c0d1ba0c1cfc6eb
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-02 16:40:24 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -11,6 +11,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//sets the public directory so it can be accessed by anything in the root directory of the app.
+// Makes css work when accessed from html file in root folder.
+app.use("/public", express.static(__dirname + "/public"));
+
 //chained middleware can just pass them one after the other to the app.get/use/put method. sets res.time
 // as string of current time in first one and responds with object { time: res.time } in 2nd one
 app.get(
@@ -28,13 +32,16 @@ app.get(
 
 //use route params to match the route and return new json obj with {2:3} after path root/2/3 entered in url
 // if we want to save to req.params must have a : before it. will save :word after first /, but not echo
-// after 2nd backslash /
+// after 2nd backslash /   Will match patterns like ./xxxxx/echo   where xxxx can be anything saved to
+//req.params object in .word property. Returned as json string rep using res.json()
 app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
-//sets the public directory so we can access with relative path
-app.use("/public", express.static(__dirname + "/public"));
+//can also match query string entered by user.
+app.get("/name?first=firstname&last=lastname", (res, req) => {
+  res.json({ name: req.params.firstname + " " + req.params.lastname });
+});
 
 //notice can't do __dirname + '/views/index.html' must do '/views/' + 'index.html'
 // also notice ; needed insinde the routeHandler since it is a function(return)
@@ -43,7 +50,10 @@ app.get("/", function routeHandler(req, res) {
   res.sendFile(__dirname + "/views/" + "index.html");
 });
 
-//NOTICE NEED ; AFTER res.json CALL SINCE IT IS CODE IN A CALLBACK FUNCTION
+//NOTICE NEED ; AFTER res.json CALL SINCE IT IS CODE IN A CALLBACK FUNCTION. res.json() responds with
+// string representation of json object passed to it. Notice uppercase needs quotes since in .env file
+// and isn't declared with const or let. Values stored in .env are stored as strings even though quotes
+// not used in their assignment in the .env file
 app.get("/json", function routeHandler(req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase")
     res.json({ message: "HELLO JSON" });
