f24ffacf5f2e2166d4adbe2723e1c579813140f9
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-03 19:05:32 -0400

    connect to mongodb

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+//connect to my mongodb deployment
 mongoose.connect(process.env.MONGOOSE_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
