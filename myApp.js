0434cafbee2698e7a1a182ebeffb0b143fce43bb
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2022-07-03 16:36:06 -0400

    msg

--- myApp.js
+++ myApp.js
@@ -72,7 +72,7 @@ app.get("/json", function routeHandler(req, res) {
 
 //body-parser allows us to return url encoded values from the form in index.html (since method is post
 //and it's action is '/name'). Form values are stored in req.body matching the <input> element's name propert
-app.post("/name", function (req, res) {
+app.post("/", function (req, res) {
   res.json({ name: req.body.first + " " + req.body.last });
 });
 
